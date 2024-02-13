ef1418aca67da0ce2bf4a766eb1c671fcacd5068
climberbrad <climberbrad@gmail.com>
2024-02-13 11:54:07 -0700

    initial commit

---
+++ Database/dbConfig.ts
@@ -0,0 +1,27 @@
+import {MongoClient, ServerApiVersion} from "mongodb";
+const uname = process.env.MONGO_USER
+const pwd = process.env.MONGO_PWD
+
+const uri = `mongodb+srv://${uname}:${pwd}@cluster0.impkuwn.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client: MongoClient = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+let conn;
+try {
+    // Connect the client to the server	(optional starting in v4.7)
+    conn = client.connect();
+} catch (e) {
+    console.error(e);
+}
+
+const dbName = "we-the-people";
+const database = client.db(dbName);
+
+export default database;
---
+++ app.ts
@@ -0,0 +1,146 @@
+import {Request, Response, Express} from "express";
+import express from 'express'
+import database from "./Database/dbConfig";
+import {WithId, Document} from "mongodb";
+import bodyParser from "body-parser";
+
+const cors = require('cors');
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json())
+
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+app.use(urlencodedParser)
+
+const PORT: Number = 3000;
+const Root: "/" = "/";
+let con: number = 0;
+let connections: any = [];
+
+const baseUrl = '/api/v1';
+
+interface Poll extends WithId<Document> {
+    id: string;
+    startDate: number;
+    question: string;
+    options: PollOption[];
+    author: string;
+    status: string;
+}
+
+export interface PollOption {
+    id: string;
+    text: string;
+    votes: number;
+}
+
+interface Vote extends WithId<Document> {
+    id: string;
+    pollId: string;
+    userId: string;
+    optionId: string;
+    date: number;
+}
+
+// Route.
+app.get(Root, async (req: Request, res: Response) => {
+    res.json({
+        data: 'success',
+        Type: true,
+    });
+})
+
+app.get(`${baseUrl}/polls`, async (req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
+    try {
+        const collectionName = "polls";
+        const collection = database.collection(collectionName);
+        const posts = await (collection.find().toArray()) as Poll[];
+
+        res.status(200).send(JSON.stringify(posts, null, 3))
+    } catch {
+        console.log('error')
+    }
+})
+
+app.get(`${baseUrl}/polls/:id`, async (req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
+    const pollId = req.params.id
+    try {
+        const collectionName = "polls";
+        const collection = database.collection(collectionName);
+        const poll = await (collection.findOne({id: pollId})) as Poll;
+
+        res.status(200).send(JSON.stringify(poll, null, 3))
+    } catch {
+        console.log('error')
+    }
+})
+
+app.post(`${baseUrl}/polls`, async (req: Request, res: Response) => {
+    // res.setHeader('Content-Type', 'application/json');
+    try {
+        const newPoll: Poll = req.body as Poll;
+
+        const collectionName = "polls";
+        const collection = database.collection(collectionName);
+        await collection.insertOne(newPoll)
+
+        res.status(200).send(newPoll)
+    } catch (e) {
+        console.log('error', e)
+    }
+})
+
+app.get(`${baseUrl}/votes`, async (req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
+    try {
+        const collectionName = "votes";
+        const collection = database.collection(collectionName);
+        const votes = await (collection.find().toArray()) as Vote[];
+
+        res.status(200).send(JSON.stringify(votes, null, 3))
+    } catch {
+        console.log('error')
+    }
+})
+
+app.post(`${baseUrl}/votes`, async (req: Request, res: Response) => {
+    // res.setHeader('Content-Type', 'application/json');
+    try {
+        const newVote: Vote = req.body as Vote;
+
+        const collectionName = "votes";
+        const collection = database.collection(collectionName);
+        await collection.insertOne(newVote)
+
+        res.status(200).send(newVote)
+    } catch (e) {
+        console.log('error', e)
+    }
+})
+
+let Server = app.listen(PORT, () => {
+    console.log("We the people API is now running on port " + PORT);
+});
+
+// Handle the connection.
+Server.on("connection", (connection: any) => {
+    connections.push(connection);
+
+    connection.on("close", function () {
+        connections = connections.filter((cur: any) => {
+            cur !== connection
+        })
+    })
+})
+
+// Close the Connection.
+connections.forEach((curr: any) => {
+    curr.close();
+});
+
+
+module.exports = {app, Server}
