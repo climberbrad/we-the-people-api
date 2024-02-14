e570a503447e2c1502b0daa32da9b6db99212a0c
climberbrad <climberbrad@gmail.com>
2024-02-14 10:00:33 -0700

    it IS alive

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response, Express} from "express";
 import {configDotenv} from "dotenv";
 import {MongoClient, ServerApiVersion} from "mongodb";
-import {Poll} from "../models/Model";
+import {Poll, Vote} from "../models/Model";
 
 configDotenv()
 
@@ -29,7 +29,18 @@ const database = client.db(dbName);
 const app: Express = express();
 app.use(express.json());
 
+const Root: "/" = "/";
 const baseUrl = '/api/v1';
+
+app.get(Root, async (req: Request, res: Response) => {
+    console.log('here')
+    res.status(200)
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        data: 'success',
+        Type: true,
+    });
+})
 
 app.get(`${baseUrl}/polls`, async (req: Request, res: Response) => {
     res.setHeader('Content-Type', 'application/json');
@@ -41,6 +52,65 @@ app.get(`${baseUrl}/polls`, async (req: Request, res: Response) => {
         res.status(200).send(JSON.stringify(posts, null, 3))
     } catch {
         console.log('error')
+    }
+})
+
+app.get(`${baseUrl}/polls/:id`, async (req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    const pollId = req.params.id
+    try {
+        const collectionName = "polls";
+        const collection = database.collection(collectionName);
+        const poll = await (collection.findOne({id: pollId})) as Poll;
+
+        res.status(200).send(JSON.stringify(poll, null, 3))
+    } catch {
+        console.log('error')
+    }
+})
+
+app.post(`${baseUrl}/polls`, async (req: Request, res: Response) => {
+    // res.setHeader('Content-Type', 'application/json');
+    try {
+        const newPoll: Poll = req.body as Poll;
+
+        const collectionName = "polls";
+        const collection = database.collection(collectionName);
+        await collection.insertOne(newPoll)
+
+        res.status(200).send(newPoll)
+    } catch (e) {
+        console.log('error', e)
+    }
+})
+
+app.get(`${baseUrl}/votes`, async (req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    try {
+        const collectionName = "votes";
+        const collection = database.collection(collectionName);
+        const votes = await (collection.find().toArray()) as Vote[];
+
+        res.status(200).send(JSON.stringify(votes, null, 3))
+    } catch {
+        console.log('error')
+    }
+})
+
+app.post(`${baseUrl}/votes`, async (req: Request, res: Response) => {
+    // res.setHeader('Content-Type', 'application/json');
+    try {
+        const newVote: Vote = req.body as Vote;
+
+        const collectionName = "votes";
+        const collection = database.collection(collectionName);
+        await collection.insertOne(newVote)
+
+        res.status(200).send(newVote)
+    } catch (e) {
+        console.log('error', e)
     }
 })
 
